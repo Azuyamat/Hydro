4256b97fcca9e04e100249114b67b787ae6e3db1
kai1846456 <kaiwright3@gmail.com>
2024-01-09 00:59:40 +0000

    feat: added message and reply

---
+++ src/main/kotlin/com/azuyamat/blixify/commands/admin/MessageCommand.kt
@@ -0,0 +1,43 @@
+package com.azuyamat.blixify.commands.admin
+
+import com.azuyamat.blixify.Formatter.format
+import com.azuyamat.blixify.commands.annotations.Catcher
+import com.azuyamat.blixify.commands.annotations.Command
+import com.azuyamat.blixify.instance
+import org.bukkit.entity.Player
+import java.util.*
+import org.bukkit.*
+
+@Command(
+    name = "message",
+    description = "Chat to other players!",
+    aliases = ["msg"],
+    permission = "blixify.dm"
+)
+val latestMessages: MutableMap<UUID, UUID> = mutableMapOf()
+
+
+class MessageCommand {
+
+    fun onCommand(player: Player, reciever: Player, @Catcher message: String) {
+        reciever.sendMessage(format("<gray>$player §<dark_gray>| §<white>$message"))
+        latestMessages.put(player.uniqueId, reciever.uniqueId)
+    }
+}
+
+    @Command(
+        name = "reply",
+        description = "Reply to messages!",
+        aliases = ["r"],
+        permission = "blixify.reply"
+    )
+
+    class ReplyCommand {
+        fun onCommand(player: Player, @Catcher message: String) {
+            var user = latestMessages.get(player.uniqueId)
+            var messager = user?.let { Bukkit.getPlayer(it) }
+            if (messager != null) {
+                messager.sendMessage(format("<gray>$player §<dark_gray>| §<white>$message"))
+            }
+        }
+    }
